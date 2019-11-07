4a80765f4f461f40db99242bb3b2445c5c9385ea
Ivan Rodriguez <ivan780@hotmail.com>
2019-11-07 16:15:54 +0100

    creacion de windows.onload, para que hasta que la "ventana" no este iniciada no se ejecute
    creacion de AppCalculator
    
    creacion del AppCalculator.constructor()
    
    creacion de la variable appCalculadora para crear AppCalculator()

---
+++ main.js
@@ -0,0 +1,11 @@
+window.onload = function () {
+    console.log("Windows.onload()");
+
+    class AppCalculator {
+        constructor() {
+            console.log("Objeto AppCalculator creado")
+        }
+    }
+
+    let appCalculadora = new AppCalculator();
+};
