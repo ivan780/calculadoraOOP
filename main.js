2f84de794fe758b7820a21a487af9d8bc2ae4747
Ivan Rodriguez <ivan780@hotmail.com>
2019-11-18 19:45:49 +0100

    creacion de la variable de ambito globar appCal fuera del windows.onload.

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
+var appCal;
 window.onload = function () {
     console.log(document.readyState);
     if (document.readyState === "complete"){
-        var pantallaHtml = document.getElementById("pantallaCalculadora");
-        var appCalculadora = new AppCalculator(document);
+        appCal = new AppCalculator(document, "pantallaCalculadora");
     }
 };
