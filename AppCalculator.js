8a6c755065194e3d0891a2ac9e529d43fdcf3399
Ivan Rodriguez <ivan780@hotmail.com>
2019-11-21 17:27:37 +0100

    funcion teclado(evento): esta funcion detecta si has pulsado una de las teclas disponibles para poder iteractuar con la calculadora sin necesidad de usar botones. funcion tecladoOperacion(tecla): esta funcion valida si el codigo de la tecla esta dentro de los codigos disponibles

--- AppCalculator.js
+++ AppCalculator.js
@@ -10,13 +10,14 @@ class AppCalculator {
         this.cookies = this.documento.cookies;
         this.avisoHtml = this.documento.getElementById("aviso");
         this.pantallaHtml = this.documento.getElementById(nombreDePantallaCalculadora);
-        this.appLocalDB = new AppLocalDB();
+        this.appLocalDB = new AppLocalDB(this.documento);
     }
 
-    init(){
+    init() {
     }
+
 //Funciones basicas/////////////////////////////////////////////////////////////////////////////////////////////////////
-     numbers(valor) {
+    numbers(valor) {
         console.log("number(" + valor + ")");
         if (this.calculo[this.posicionCalculo] != 0 || this.calculo[this.posicionCalculo] == "") {
             this.calculo[this.posicionCalculo] += valor;
@@ -26,8 +27,9 @@ class AppCalculator {
             this.toScreen(true);
         }
     }
+
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-     operaciones(operacion) {
+    operaciones(operacion) {
         console.log("operaciones(" + operacion + ")");
         if (this.imparCalculo() && this.calculo[this.posicionCalculo] != "") {
             this.posicionCalculo++;
@@ -35,35 +37,39 @@ class AppCalculator {
             this.posicionCalculo++;
             this.calculo[this.posicionCalculo] = "";
             this.toScreen(true);
-        }else {
+        } else {
             this.avisoHtml.innerText = "Tienes que introducir un numero para hacer una operacion";
         }
     }
+
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-     punto(valor) {
-        if (this.calculo[this.posicionCalculo] != "" && valor == "."){
-            if (this.tienePunto()){
+    punto(valor) {
+        if (this.calculo[this.posicionCalculo] != "" && valor == ".") {
+            if (this.tienePunto()) {
                 this.numbers(valor);
             }
         }
     }
+
 //Funciones del codigo//////////////////////////////////////////////////////////////////////////////////////////////////
-     imparCalculo() {
+    imparCalculo() {
         console.log("parCalculo()");
         console.log("-" + (this.calculo.length % 2 === 1));
         return this.calculo.length % 2 === 1;
     }
-     tienePunto() {
+
+    tienePunto() {
         var i = this.calculo[this.posicionCalculo].indexOf(".");
-        if (i == -1){
+        if (i == -1) {
             return true
-        }else {
+        } else {
             return false
         }
 
     }
+
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-     toScreen(modo) {
+    toScreen(modo) {
         var tScreen;
 
         for (var i = 0; i < this.calculo.length; i++) {
@@ -74,54 +80,80 @@ class AppCalculator {
                 tScreen += ' ' + this.calculo[i];
             }
         }
-        if (modo){
+        if (modo) {
             this.pantallaHtml.value = tScreen;
-        }else {
+        } else {
             return tScreen;
         }
     }
+
 //Funciones de calculadora//////////////////////////////////////////////////////////////////////////////////////////////
-     clean(){
-         this.calculo = [""];
-         this.posicionCalculo = 0;
-         this.toScreen(true);
+    clean() {
+        this.calculo = [""];
+        this.posicionCalculo = 0;
+        this.toScreen(true);
     }
-     fullClean() {
-         this.calculo = [""];
-         this.posicionCalculo = 0;
-         this.toScreen(true);
-         this.cookies = "";
+
+    fullClean() {
+        this.calculo = [""];
+        this.posicionCalculo = 0;
+        this.toScreen(true);
+        this.cookies = "";
     }
+
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-     backspace() {
+    backspace() {
         console.log("backspace()");
         console.log("-" + this.calculo[this.posicionCalculo]);
         calculo[this.posicionCalculo] = this.calculo[this.posicionCalculo].slice(0, -1);
         console.log("-" + this.calculo[this.posicionCalculo]);
-         this.toScreen(true);
+        this.toScreen(true);
     }
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-     resultado() {
-        if (this.calculo[this.posicionCalculo] != ""){
+    resultado() {
+        if (this.calculo[this.posicionCalculo] != "") {
             this.calculo = [eval(this.toScreen(false))];
             this.posicionCalculo = 0;
             this.pantallaHtml.value = this.calculo[this.posicionCalculo];
         }
     }
+
 //Funciones de memoria//////////////////////////////////////////////////////////////////////////////////////////////////
-     memoriaView() {
+    memoriaView() {
         console.log("memoriaview()");
         var memoria = this.appLocalDB.getCookies();
         console.log("-Memoria = " + memoria);
-        if (memoria != ""){
+        if (memoria != "") {
             this.numbers(memoria);
         }
     }
+
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-     memoriaSave() {
+    memoriaSave() {
         console.log("memoriaSave()");
         console.log("-memoria=" + this.toScreen(false));
         this.appLocalDB.setCookies(this.toScreen(false));
+    }
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    teclado(evento) {
+        console.log("tecladito(" + evento.keyCode + ")");
+        if (evento.key <= 9 || evento.key >= 0) {
+            this.numbers(evento.key);
+        }else if (this.tecladoOperacion(evento.keyCode)){
+            this.operaciones(evento.key);
+        }else if (evento.keyCode == 13){
+            this.resultado();
+        }
+
+
+    }
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    tecladoOperacion(tecla){
+        if (tecla == 43|| tecla == 45|| tecla == 42|| tecla == 47){
+            return true;
+        }
+        return false;
     }
 }
